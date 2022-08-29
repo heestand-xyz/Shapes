97bab3931149861362c979cfe2c457b5e7242533
Anton Heestand <anton.heestand@gmail.com>
2022-08-29 12:34:43 +0200

    Rounded Arc

---
+++ Sources/Shapes/CGRect+center.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anton Heestand on 2022-08-29.
+//
+
+import CoreGraphics
+
+extension CGRect {
+    
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+}
--- Sources/Shapes/RoundedArc.swift
+++ Sources/Shapes/RoundedArc.swift
@@ -86,10 +86,3 @@ public struct RoundedArc: Shape {
         }
     }
 }
-
-extension CGRect {
-    
-    var center: CGPoint {
-        CGPoint(x: midX, y: midY)
-    }
-}
