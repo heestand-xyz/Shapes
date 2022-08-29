5d4b80f7b2d52d61887fdfb41f14347ac15edb99
Anton Heestand <anton.heestand@gmail.com>
2022-08-29 20:27:43 +0200

    The Circle

---
+++ Sources/Shapes/Dount.swift
@@ -0,0 +1,35 @@
+//
+//  Dount.swift
+//  
+//
+//  Created by Anton Heestand on 2022-08-29.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct Dount: Shape {
+    
+    let width: CGFloat
+    
+    public init(width: CGFloat) {
+        self.width = width
+    }
+    
+    public func path(in rect: CGRect) -> Path {
+        
+        Path { path in
+            
+            path.addArc(center: rect.center, radius: rect.width / 2, startAngle: .zero, endAngle: Angle(degrees: 360), clockwise: true)
+            
+            path.addArc(center: rect.center, radius: rect.width / 2 - width, startAngle: .zero, endAngle: Angle(degrees: 360), clockwise: false)
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+struct Dount_Previews: PreviewProvider {
+    static var previews: some View {
+        Dount(width: 50)
+    }
+}
